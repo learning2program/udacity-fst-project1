218ef2034e31268c78bf9229af28afb2134177d7
learning2program <haumaijeet@gmail.com>
2015-02-22 11:58:41 -0800

    format adjustments

--- entertainment_center.py
+++ entertainment_center.py
@@ -15,15 +15,12 @@ Unbroken = media.Movie(
     "Unbroken",
      "As a boy, Louie Zamperini is always in trouble, but with the help of his older brother he turns his life around and channels his energy into running, later qualifying for the 1936 Olympics. When World War II breaksout, Louie enlists in the military. After his plane crashes in the Pacific, he survives an incredible 47 days adrift in a raft, until his capture by the Japanese navy. Sent to a POW camp, Louie becomes the favorite target of a particularly cruel prison commander.",
      "http://upload.wikimedia.org/wikipedia/en/7/76/Unbroken_poster.jpg",
-     "http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dkk1M_HwmFMM&ei=g2nlVMbqIoKkoQSU6IDQAQ&usg=AFQjCNHhVWyq92Fodphb6TPEYvskF69HVA&sig2=o39OFm_K0HoG_HJEDIFxfg&bvm=bv.86475890,d.cGU"
-)
+     "http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dkk1M_HwmFMM&ei=g2nlVMbqIoKkoQSU6IDQAQ&usg=AFQjCNHhVWyq92Fodphb6TPEYvskF69HVA&sig2=o39OFm_K0HoG_HJEDIFxfg&bvm=bv.86475890,d.cGU")
 
 everything_is_illuminated = media.Movie("Everything is Illuminated",
                                         "A young Jewish-American man obsessed with his family history, ",
                                         "http://upload.wikimedia.org/wikipedia/en/2/27/Everything_Is_Illuminated_film.jpg",
-                                        "https://www.youtube.com/watch?v=tSUOYY4oukc"
-
-)
+                                        "https://www.youtube.com/watch?v=tSUOYY4oukc")
 
 movies = [ shawshank_redemption, Unbroken, everything_is_illuminated  ]
 fresh_tomatoes.open_movies_page(movies)
